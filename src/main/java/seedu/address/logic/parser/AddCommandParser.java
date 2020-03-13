9d2c8a39fdda1faad3dc0b9fb6449dad9eb693cd
Yonggiee <fongyongjie27@gmail.com>
2020-03-13 22:24:26 +0800

    remove Gender attributes

--- src/main/java/seedu/address/logic/parser/AddCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddCommandParser.java
@@ -33,21 +33,20 @@ public class AddCommandParser implements Parser<AddCommand> {
      */
     public AddCommand parse(String args) throws ParseException {
         ArgumentMultimap argMultimap =
-                ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS, PREFIX_TAG, PREFIX_GENDER);
+                ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS, PREFIX_TAG);
 
-        if (!arePrefixesPresent(argMultimap, PREFIX_NAME, PREFIX_ADDRESS, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_GENDER)
+        if (!arePrefixesPresent(argMultimap, PREFIX_NAME, PREFIX_ADDRESS, PREFIX_PHONE, PREFIX_EMAIL)
                 || !argMultimap.getPreamble().isEmpty()) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
         }
 
         Name name = ParserUtil.parseName(argMultimap.getValue(PREFIX_NAME).get());
-        Gender gender = ParserUtil.parseGender(argMultimap.getValue(PREFIX_GENDER).get());
         Phone phone = ParserUtil.parsePhone(argMultimap.getValue(PREFIX_PHONE).get());
         Email email = ParserUtil.parseEmail(argMultimap.getValue(PREFIX_EMAIL).get());
         Address address = ParserUtil.parseAddress(argMultimap.getValue(PREFIX_ADDRESS).get());
         Set<Tag> tagList = ParserUtil.parseTags(argMultimap.getAllValues(PREFIX_TAG));
 
-        Client client = new Client(name, gender, phone, email, address, tagList);
+        Client client = new Client(name, phone, email, address, tagList);
 
         return new AddCommand(client);
     }
--- src/main/java/seedu/address/model/client/Client.java
+++ src/main/java/seedu/address/model/client/Client.java
@@ -86,9 +86,8 @@ public class Client {
 
         return otherClient != null
                 && otherClient.getName().equals(getName())
-                && (otherClient.getPhone().equals(getPhone()) 
-                || otherClient.getEmail().equals(getEmail())
-                || otherClient.getGender().equals(getGender()));
+                && (otherClient.getPhone().equals(getPhone())
+                || otherClient.getEmail().equals(getEmail()));
     }
 
     /**
@@ -107,7 +106,6 @@ public class Client {
 
         Client otherClient = (Client) other;
         return otherClient.getName().equals(getName())
-                && otherClient.getGender().equals(getGender())
                 && otherClient.getPhone().equals(getPhone())
                 && otherClient.getEmail().equals(getEmail())
                 && otherClient.getAddress().equals(getAddress())
@@ -117,15 +115,13 @@ public class Client {
     @Override
     public int hashCode() {
         // use this method for custom fields hashing instead of implementing your own
-        return Objects.hash(name, gender, phone, email, address, tags);
+        return Objects.hash(name, phone, email, address, tags);
     }
 
     @Override
     public String toString() {
         final StringBuilder builder = new StringBuilder();
         builder.append(getName())
-                .append(" Gender: ")
-                .append(getGender())
                 .append(" Phone: ")
                 .append(getPhone())
                 .append(" Email: ")
