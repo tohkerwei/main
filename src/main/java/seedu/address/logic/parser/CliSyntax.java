207e3ac1ae84f1df5e7226968fcfff01239d06ca
Yonggiee <fongyongjie27@gmail.com>
2020-03-13 21:19:02 +0800

    remove RemarkCommand

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -11,5 +11,6 @@ public class CliSyntax {
     public static final Prefix PREFIX_EMAIL = new Prefix("e/");
     public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
     public static final Prefix PREFIX_REMARK = new Prefix("r/");
+    public static final Prefix PREFIX_GENDER = new Prefix("g/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
 }
