edfdc1590c3550124160e678dd50ceb6864ebaf1
Aaron Choo <choozy.aaron@gmail.com>
2020-03-19 09:34:35 +0800

    Change messages from AB3 to FitBiz

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -71,7 +71,7 @@ public class EditCommand extends Command {
 
     public static final String MESSAGE_EDIT_CLIENT_SUCCESS = "Edited Client: %1$s";
     public static final String MESSAGE_NOT_EDITED = "At least one field to edit must be provided.";
-    public static final String MESSAGE_DUPLICATE_CLIENT = "This client already exists in the address book.";
+    public static final String MESSAGE_DUPLICATE_CLIENT = "This client already exists in FitBiz.";
 
     private final Index index;
     private final EditClientDescriptor editClientDescriptor;
