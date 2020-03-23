a705458a206fd9cfd3baae27d52738b336707909
Dban1 <huggavamp@gmail.com>
2020-03-23 21:39:06 +0800

    Pre-emptively finished up the basics and checkstyling for Schedule and DayEnum.

--- src/main/java/seedu/address/model/schedule/DayEnum.java
+++ src/main/java/seedu/address/model/schedule/DayEnum.java
@@ -1,5 +1,11 @@
 package seedu.address.model.schedule;
 
+/**
+ * Wrapper class for WeekDay enum.
+ */
 public class DayEnum {
-    public enum WEEKDAY {SUN, MON, TUE, WED, THU, FRI, SAT}
+    /**
+     * Enumerates the seven days of the week.
+     */
+    public enum WeekDay { SUN, MON, TUE, WED, THU, FRI, SAT }
 }
