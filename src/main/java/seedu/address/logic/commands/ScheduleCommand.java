d87ad1298d5a550e94bfe32bca58b6a530148ca4
Dban1 <huggavamp@gmail.com>
2020-03-26 16:01:44 +0800

    Added ScheduleCommand to AddressBookParser and slightly formatted Schedule command usage instructions.

--- src/main/java/seedu/address/logic/commands/ScheduleCommand.java
+++ src/main/java/seedu/address/logic/commands/ScheduleCommand.java
@@ -24,7 +24,8 @@ public class ScheduleCommand extends Command {
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds the training schedule of the client, identified "
             + "by the index number used in the displayed client list. "
-            + "The schedule should include the first 3 letters of the day, start and end time in 24 hour format, "
+            + "The schedule should include the first 3 letters of the day, \n"
+            + "start and end time in 24 hour format, "
             + "in 30 minutes denominations. Multiple training schedule can be added to a client.\n"
             + "Parameters: INDEX (a positive integer) d/DAY st/START_TIME et/END_TIME [MORE_SCHEDULE]...\n"
             + "Example: " + COMMAND_WORD + " 1 d/mon st/1200 et/1400 d/fri st/1330 et/1500";
