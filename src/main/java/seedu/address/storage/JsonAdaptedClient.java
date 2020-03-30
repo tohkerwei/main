76a00915a846ec1a91218170e61ed690a074904a
Aaron Choo <choozy.aaron@gmail.com>
2020-03-30 17:35:26 +0800

    Revert "Ensure exercise list is sorted after reading from storage"
    This reverts commit c7e2940782a30a502b73e382a39c183548514d7b.

--- src/main/java/seedu/address/storage/JsonAdaptedClient.java
+++ src/main/java/seedu/address/storage/JsonAdaptedClient.java
@@ -129,7 +129,6 @@ class JsonAdaptedClient {
         for (JsonAdaptedExercise ex : exerciseList) {
             modelExerciseList.add(ex.toModelType());
         }
-        modelExerciseList.sortByExerciseDate(); // do a sanity presort to ensure initial list is sorted
 
         if (name == null) {
             throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, Name.class.getSimpleName()));
