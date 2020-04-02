61b8a586bf7e9ac6c7405631f6763fa66ae3df2d
toh ker wei <mrtohkerwei@gmail.com>
2020-04-02 23:35:45 +0800

    Fix

--- src/test/java/seedu/address/logic/parser/ScheduleCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/ScheduleCommandParserTest.java
@@ -1,30 +1,20 @@
 package seedu.address.logic.parser;
 
-import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-
-import static seedu.address.logic.commands.CommandTestUtil.DAY_DESC_MON;
-import static seedu.address.logic.commands.CommandTestUtil.INVALID_DAY_DESC;
-import static seedu.address.logic.commands.CommandTestUtil.INVALID_ENDTIME_DESC;
-import static seedu.address.logic.commands.CommandTestUtil.INVALID_STARTTIME_DESC;
-import static seedu.address.logic.commands.CommandTestUtil.TIME_DESC_END;
-import static seedu.address.logic.commands.CommandTestUtil.TIME_DESC_START;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_DAY_MON;
-
-import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
-import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
-import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_CLIENT;
-
-import java.util.ArrayList;
-
 import org.junit.jupiter.api.Test;
-
 import seedu.address.commons.core.index.Index;
-import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.ScheduleCommand;
 import seedu.address.model.schedule.Day;
 import seedu.address.model.schedule.EndTime;
 import seedu.address.model.schedule.Schedule;
 import seedu.address.model.schedule.StartTime;
+
+import java.util.ArrayList;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.commands.CommandTestUtil.*;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_CLIENT;
 
 public class ScheduleCommandParserTest {
 
@@ -35,7 +25,8 @@ public class ScheduleCommandParserTest {
 
     @Test
     public void parse_emptyArg_throwsParseException() {
-        assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, "     ", String.format(
+                MESSAGE_INVALID_COMMAND_FORMAT, ScheduleCommand.MESSAGE_USAGE));
     }
 
     @Test
@@ -54,7 +45,7 @@ public class ScheduleCommandParserTest {
         String userInput = targetIndex.getOneBased() + DAY_DESC_MON + TIME_DESC_START + TIME_DESC_END;
 
         ArrayList<Schedule> scheduleArrayList = new ArrayList<Schedule>();
-        scheduleArrayList.add(Schedule.stringToSchedule("day/mon st/1200 et/1400"));
+        scheduleArrayList.add(new Schedule(new Day("mon"), new StartTime("1200"), new EndTime("1400")));
         ScheduleCommand expectedCommand = new ScheduleCommand(targetIndex, scheduleArrayList);
         assertParseSuccess(parser, userInput, expectedCommand);
     }
@@ -71,8 +62,8 @@ public class ScheduleCommandParserTest {
 
     @Test
     public void parse_invalidValue_failure() {
-        assertParseFailure(parser, "1" + INVALID_DAY_DESC, Day.MESSAGE_CONSTRAINTS); // invalid day
-        assertParseFailure(parser, "1" + INVALID_STARTTIME_DESC, StartTime.MESSAGE_CONSTRAINTS); // invalid start time
-        assertParseFailure(parser, "1" + INVALID_ENDTIME_DESC, EndTime.MESSAGE_CONSTRAINTS); // invalid end time
+        assertParseFailure(parser, "1" + INVALID_DAY_DESC, Schedule.MESSAGE_CONSTRAINTS); // invalid day
+        assertParseFailure(parser, "1" + INVALID_STARTTIME_DESC, Schedule.MESSAGE_CONSTRAINTS); // invalid start time
+        assertParseFailure(parser, "1" + INVALID_ENDTIME_DESC, Schedule.MESSAGE_CONSTRAINTS); // invalid end time
     }
 }
