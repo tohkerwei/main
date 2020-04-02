bbc281b456273340f8197bf320ab9200a6f87193
Dban1 <huggavamp@gmail.com>
2020-04-03 00:06:54 +0800

    Reinstated previously disabled.

--- src/test/java/seedu/address/storage/JsonAdaptedScheduleTest.java
+++ src/test/java/seedu/address/storage/JsonAdaptedScheduleTest.java
@@ -1,21 +1,28 @@
 package seedu.address.storage;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.schedule.Schedule;
+import seedu.address.testutil.ScheduleBuilder;
+
 class JsonAdaptedScheduleTest {
 
-//    private final static String INVALID_DAY = "RANDOMDAY";
-//    private final static String INVALID_START_TIME = "55:55";
-//    private final static String INVALID_END_TIME = "99:99";
-//
-//    private final static String VALID_DAY = "MON";
-//    private final static String VALID_START_TIME = "11:00";
-//    private final static String VALID_END_TIME = "12:00";
+    private static final String INVALID_DAY = "RANDOMDAY";
+    private static final String INVALID_START_TIME = "55:55";
+    private static final String INVALID_END_TIME = "99:99";
 
-//    @Test
-//    public void toModelType_validSchedule_returnsSchedule() {
-//        Schedule testSchedule = new ScheduleBuilder().withDay(VALID_DAY).withStartTime(VALID_START_TIME)
-//                .withEndTime(VALID_END_TIME).build();
-//        JsonAdaptedSchedule schedule = new JsonAdaptedSchedule(testSchedule);
-//
-//        assertEquals(testSchedule, schedule);
-//    }
+    private static final String VALID_DAY = "MON";
+    private static final String VALID_START_TIME = "11:00";
+    private static final String VALID_END_TIME = "12:00";
+
+    @Test
+    public void toModelType_validSchedule_returnsSchedule() {
+        Schedule testSchedule = new ScheduleBuilder().withDay(VALID_DAY).withStartTime(VALID_START_TIME)
+                .withEndTime(VALID_END_TIME).build();
+        JsonAdaptedSchedule schedule = new JsonAdaptedSchedule(testSchedule);
+
+        assertEquals(testSchedule, schedule);
+    }
 }
