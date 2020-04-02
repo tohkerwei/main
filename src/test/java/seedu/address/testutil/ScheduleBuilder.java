3c019e9c96ee7f3a4c60d3a3a432f9c1409279da
Dban1 <huggavamp@gmail.com>
2020-04-02 23:39:14 +0800

    no message

--- src/test/java/seedu/address/testutil/ScheduleBuilder.java
+++ src/test/java/seedu/address/testutil/ScheduleBuilder.java
@@ -2,7 +2,7 @@ package seedu.address.testutil;
 
 import seedu.address.model.schedule.Day;
 import seedu.address.model.schedule.EndTime;
-import seedu.address.model.schedule.Schedule;
+//import seedu.address.model.schedule.Schedule;
 import seedu.address.model.schedule.StartTime;
 
 /**
@@ -23,24 +23,30 @@ public class ScheduleBuilder {
         startTime = new StartTime(DEFAULT_START_TIME);
         endTime = new EndTime(DEFAULT_END_TIME);
     }
-
-    public Schedule build() {
-        return new Schedule(day, startTime, endTime);
-    }
-
-    public ScheduleBuilder withDay(String day) {
-        this.day = new Day(day);
-        return this;
-    }
-
-    public ScheduleBuilder withStartTime(String st) {
-        this.startTime = new StartTime(st);
-        return this;
-    }
-
-    public ScheduleBuilder withEndTime(String et) {
-        this.endTime = new EndTime(et);
-        return this;
-    }
+//
+//    public Schedule build() {
+//        return new Schedule(day, startTime, endTime);
+//    }
+//
+//    public ScheduleBuilder withDay(String day) {
+//        this.day = new Day(day);
+//        return this;
+//    }
+//
+//
+//    public ScheduleBuilder withStartTime(String st) {
+//        this.startTime = new StartTime(st);
+//        return this;
+//    }
+//
+//    /**
+//     * Adds endtime to the schedulebuilder chain.
+//     * @param et
+//     * @return
+//     */
+//    public ScheduleBuilder withEndTime(String et) {
+//        this.endTime = new EndTime(et);
+//        return this;
+//    }
 
 }
