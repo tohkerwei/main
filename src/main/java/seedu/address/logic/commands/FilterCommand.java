11eabaa5795136b1d00bf6762ba3c6587a975c9b
toh ker wei <mrtohkerwei@gmail.com>
2020-04-06 16:00:22 +0800

    Added filter-c command that can filter tags

---
+++ src/main/java/seedu/address/logic/commands/FilterCommand.java
@@ -0,0 +1,45 @@
+package seedu.address.logic.commands;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.model.Model;
+import seedu.address.model.client.TagContainsKeywordsPredicate;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+
+/**
+ * @author tohkerwei
+ * Filters and lists all clients in FitBiz whose tag contains any of the argument keywords.
+ * Keyword matching is case insensitive.
+ */
+public class FilterCommand extends Command{
+
+    public static final String COMMAND_WORD = "filter-c";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Filters and display all clients whose tag contains "
+            + "any of the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
+            + "Parameters: t/KEYWORD [MORE_KEYWORDS]...\n"
+            + "Example: " + COMMAND_WORD + PREFIX_TAG + " vegetarian overweight handsome";
+
+    private final TagContainsKeywordsPredicate predicate;
+
+    public FilterCommand(TagContainsKeywordsPredicate predicate) {
+        this.predicate = predicate;
+    }
+
+    @Override
+    public CommandResult execute(Model model) {
+        requireNonNull(model);
+        model.updateFilteredClientList(predicate);
+        return new CommandResult(
+                String.format(Messages.MESSAGE_CLIENTS_LISTED_OVERVIEW, model.getFilteredClientList().size()));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof FilterCommand // instanceof handles nulls
+                && predicate.equals(((FilterCommand) other).predicate)); // state check
+    }
+}
+
