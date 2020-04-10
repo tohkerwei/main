dfd46840a24e975fb3ec4fc4fb7de37ddd081892
Aaron Choo <choozy.aaron@gmail.com>
2020-04-10 09:43:33 +0800

    Fix error message

--- src/main/java/seedu/address/logic/commands/ExportCommand.java
+++ src/main/java/seedu/address/logic/commands/ExportCommand.java
@@ -1,5 +1,6 @@
 package seedu.address.logic.commands;
 
+import static seedu.address.logic.export.Exporter.DEFAULT_EXERCISES_DIRECTORY;
 import static java.util.Objects.requireNonNull;
 
 import seedu.address.logic.commands.exceptions.CommandException;
@@ -17,10 +18,11 @@ public class ExportCommand extends Command {
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Exports the exercises of the client in view.";
 
-    public static final String MESSAGE_SUCCESS = "Succesfully exported this client's exercises to '%s'.\n"
-            + "Please check your /exports directory.";
+    public static final String MESSAGE_SUCCESS = "Succesfully exported this client's exercises to \"%s\".\n"
+            + String.format("Please check your /%s directory.", DEFAULT_EXERCISES_DIRECTORY);
 
-    public static final String MESSAGE_CLIENT_NOT_IN_VIEW = "You currently do not have a client in view, ";
+    public static final String MESSAGE_CLIENT_NOT_IN_VIEW = "You currently do not have a client in view, "
+            + "use the view-c command to view a client first";
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
